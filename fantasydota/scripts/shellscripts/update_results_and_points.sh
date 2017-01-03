407161ba0beaf4a8b3ea15fc260bb785e212d825
ThePianoDentist <jbknight07@gmail.com>
2017-01-03 20:18:57 +0000

    Hmm i think I broke git

--- fantasydota/scripts/shellscripts/update_results_and_points.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-~/env/bin/python ~/env/bin/fantasydota/fantasydota/scripts/update_results_and_points.py
--- fantasydota/scripts/shellscripts/update_results_and_points_local.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-~/bin/seovenv/bin/python ~/bin/seovenv/bin/fantasydota/fantasydota/scripts/update_results_and_points.py
