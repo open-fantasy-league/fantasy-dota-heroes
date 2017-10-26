8e5ca1fc81462dc37c69122dc0b4145481cc25f0
ThePianoDentist <jbknight07@gmail.com>
2017-10-26 15:17:06 +0100

    fix addheroplayer to work with reserves

--- fantasydota/scripts/tmpscripts/add_hero_player.py
+++ fantasydota/scripts/tmpscripts/add_hero_player.py
@@ -16,7 +16,7 @@ def add_hero_player(session, user_id, hero_id, league_id, reserve):
         hero_value = session.query(Hero.value).filter(and_(Hero.id == hero_id,
                                                            Hero.league == league_id)).first()[0]
 
-        teamq = session.query(TeamHero).filter(TeamHero.user_id == user_id).filter(TeamHero.league == league_id)
+        teamq = session.query(TeamHero).filter(TeamHero.user_id == user_id).filter(TeamHero.league == league_id).filter(TeamHero.reserve.is_(reserve))
         teamq_hero = teamq.filter(TeamHero.hero_id == hero_id)
 
         l_user = session.query(LeagueUser).filter(LeagueUser.user_id == user_id).filter(
@@ -25,6 +25,7 @@ def add_hero_player(session, user_id, hero_id, league_id, reserve):
         user_money = l_user.money
 
         if user_money < hero_value:
+            print("ERROR: Insufficient credits")
             return {"success": False, "message": "ERROR: Insufficient credits"}
 
         new_credits = round(user_money - hero_value, 1)
@@ -32,6 +33,7 @@ def add_hero_player(session, user_id, hero_id, league_id, reserve):
         if teamq.count() >= 5:
             return {"success": False, "message": "ERROR: Team is currently full"}
         if teamq_hero.first():
+            print "ERROR: Hero already in team"
             return {"success": False, "message": "ERROR: Hero already in team"}
         else:
             l_user.money = new_credits
@@ -43,11 +45,11 @@ def add_hero_player(session, user_id, hero_id, league_id, reserve):
 
 def main():
     session = make_session()
-    add_hero_player(session, 480, 107, 5609, False)
-    add_hero_player(session, 480, 71, 5609, False)
-    add_hero_player(session, 480, 112, 5609, False)
-    add_hero_player(session, 480, 66, 5609, False)
-    add_hero_player(session, 480, 32, 5609, False)
+    # add_hero_player(session, 480, 107, 5609, False)
+    # add_hero_player(session, 480, 71, 5609, False)
+    # add_hero_player(session, 480, 112, 5609, False)
+    # add_hero_player(session, 480, 66, 5609, False)
+    # add_hero_player(session, 480, 32, 5609, False)
 
     add_hero_player(session, 480, 30, 5609, True)
     add_hero_player(session, 480, 18, 5609, True)
