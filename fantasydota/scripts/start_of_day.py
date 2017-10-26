8a18927ac9dcd8f7a87a74f5d05b2f930b8caaa0
ThePianoDentist <jbknight07@gmail.com>
2017-10-26 13:31:12 +0100

    only do incomplete team reset on after first day

--- fantasydota/scripts/start_of_day.py
+++ fantasydota/scripts/start_of_day.py
@@ -6,12 +6,10 @@ from fantasydota.models import League
 def start_of_day():
     session = make_session(transaction=False, autoflush=False)
     for league in session.query(League).all():  # .filter(League.status == 1)
-        # session.query(League).filter(League.id == league.id).update({
-        #     League.transfer_open: 0
-        # })
         league.transfer_open = False  # close league window if not already closed
         league.swap_open = False
-        reset_incomplete_teams(session, league)
+        if league.current_day > 0:
+            reset_incomplete_teams(session, league)
     session.commit()
 
 
