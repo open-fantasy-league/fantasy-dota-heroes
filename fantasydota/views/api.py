5d80939f5675691a5a20e429aaf76bff35cde5dd
ThePianoDentist <jbknight07@gmail.com>
2018-01-28 22:22:59 +0100

    fix api for hero

--- fantasydota/views/api.py
+++ fantasydota/views/api.py
@@ -6,6 +6,6 @@ from pyramid.view import view_config
 @view_config(route_name="hero", renderer="json")
 def hero(request):
     session = DBSession()
-    week = int(request.args.get('week'))
+    week = int(request.params.get('week', '1'))
     hero_values = session.query(Hero).filter(Hero.league == week).all()
     return {'week': week, 'heroes': hero_values}
