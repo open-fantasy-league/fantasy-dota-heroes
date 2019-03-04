3f91f9390e8aca34e4f770b853dc74c088998dcd
ThePianoDentist <jbknight07@gmail.com>
2019-03-04 22:05:49 +0000

    changes to work locally

--- fantasydota/lib/constants.py
+++ fantasydota/lib/constants.py
@@ -7,7 +7,7 @@ SECONDS_IN_12_HOURS = 43200
 SECONDS_IN_HOUR = 3600
 
 DEFAULT_LEAGUE = 1
-API_URL = 'https://fantasyesport.eu/api/v1/'
+API_URL = 'http://localhost/api/v1/'
 
 FESPORT_ACCOUNT = 0
 STEAM_ACCOUNT = 1
