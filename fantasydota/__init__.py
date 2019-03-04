9bc35792cea5a3461a87764c346c17a50880153a
ThePianoDentist <jbknight07@gmail.com>
2019-03-04 22:49:34 +0000

    readd hall of fame route

--- fantasydota/__init__.py
+++ fantasydota/__init__.py
@@ -82,6 +82,7 @@ def main(global_config, **settings):
     config.add_route('login', '/login')
     config.add_route('logout', '/logout')
     config.add_route('view_faq', '/faq')
+    config.add_route('hall_of_fame', '/hallOfFame')
     
     config.add_route('register', '/register')
     config.add_route('change_password', '/changePassword')
